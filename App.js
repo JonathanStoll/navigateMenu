55fe9320a6a0742843d03e57423f31f75bcef118
JonathanStoll <jonafstoll@gmail.com>
2024-02-17 21:56:26 -0300

    grid list and some styles

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
-  return (
+  return (<>
+    <StatusBar style='light' />
     <CategoriesScreen />
+  </>
   );
 }
 
