23752d2d8b460cf3a79e000623f09d9fead69fa1
JonathanStoll <jonafstoll@gmail.com>
2024-02-19 15:53:37 -0300

    styling meal item component

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,10 +1,74 @@
-import { Text, View } from "react-native"
+import {
+  Text,
+  View,
+  Pressable,
+  Image,
+  StyleSheet,
+  Platform,
+} from "react-native";
 
-function MealItem({title}){
-    return <View>
-        <Text>{title}</Text>
+function MealItem({ title, imageUrl, complexity, duration, affordability }) {
+  return (
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.detailes}>
+            <Text style={styles.detailItem}>{duration} m</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
+  );
+}
 
-  }
+export default MealItem;
 
-  export default MealItem
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.select({ ios: "visible", android: "hidden" }),
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  detailes: {
+    margin: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
--- screens/mealsScreen.js
+++ screens/mealsScreen.js
@@ -8,16 +8,24 @@ function MealsScreen({ route }) {
     return mealItem.categoryIds.indexOf(catID) >= 0;
   });
 
-  function renderMealItem(itemData){
-    return <MealItem title={itemData.item.title} />
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      complexity: item.complexity,
+      duration: item.duration,
+      affordability: item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
     <View style={styles.container}>
-      <FlatList 
-      data={displayMeals}
-      keyExtractor={(item)=>item.id}
-      renderItem={renderMealItem}
+      <FlatList
+        data={displayMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
       />
     </View>
   );
@@ -28,5 +36,6 @@ export default MealsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
   },
 });
